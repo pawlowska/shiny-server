f4b2e76a0d40916ee37845cb85417abdb40ae95f
Monika Pawlowska <github@greenelephant.pl>
2017-09-03 13:20:57 +0200

    Revert "porzadki w plikach z danymi"
    This reverts commit 2fade97722b6d22615a96e66486fdbe90b5d4bc0.

--- rowery/zaladuj_dane_z_plikow.R
+++ rowery/zaladuj_dane_z_plikow.R
@@ -5,24 +5,24 @@ source('read_from_api.R', encoding = 'UTF-8')
 source('obsluga_sumowania.R', encoding = 'UTF-8')
 
 #dane polaczone z danych godzinowych
-dane_dolutego2017<-fread("dane_zrodlowe/dane-20140813-20170202-poprSw.csv", encoding = 'UTF-8')
+dane_dolutego2017<-fread("dane/dane-20140813-20170202-poprSw.csv", encoding = 'UTF-8')
 dane_dolutego2017[,Data := as.Date(Data, tz="Europe/Berlin")]
 
-dane_domarca2017<-zaladuj_dane('dane_zrodlowe/2017styczen_marzecutf8-poprSw.csv') 
+dane_domarca2017<-zaladuj_dane('dane/2017styczen_marzecutf8-poprSw.csv') 
 setnames(dane_domarca2017, grep("Praska", names(dane_domarca2017), value = TRUE), grep("Praska", names(dane_dolutego2017), value = TRUE))
 setnames(dane_domarca2017, names(dane_domarca2017), names(dane_dolutego2017))
 
 dane_od2014_domarca2017<-rbind(dane_dolutego2017[Data<min(dane_domarca2017[,Data]),], dane_domarca2017)
 
 #polacz kwiecien i czerwiec
-dane_dokwietnia2017<-zaladuj_dane('dane_zrodlowe/2017marzec_kwiecien_utf8_poprPZ-poprSw.csv', zwirki_i_wigury = FALSE) 
-dane_doczerwca2017<-zaladuj_dane('dane_zrodlowe/2017kwiecien-czerwiec_utf8-poprSw.csv', zwirki_i_wigury = FALSE) 
+dane_dokwietnia2017<-zaladuj_dane('dane/2017marzec_kwiecien_utf8_poprPZ-poprSw.csv', zwirki_i_wigury = FALSE) 
+dane_doczerwca2017<-zaladuj_dane('dane/2017kwiecien-czerwiec_utf8-poprSw.csv', zwirki_i_wigury = FALSE) 
 
 dane_odmarca_doczerwca2017<-rbind(dane_dokwietnia2017[Data<min(dane_doczerwca2017[,Data]),], dane_doczerwca2017)
 dane_odmarca_doczerwca2017[,"Praska sciezka rekreacyjna":=NULL]
 setnames(dane_odmarca_doczerwca2017, "Rowery", grep("Praska", names(dane_dolutego2017), value = TRUE))
 
-dane_dolipca2017<-zaladuj_dane('dane_zrodlowe/2017_czerwiec-lipiec_poprPZ-poprSw.csv', zwirki_i_wigury = FALSE) 
+dane_dolipca2017<-zaladuj_dane('dane/2017_czerwiec-lipiec_poprPZ-poprSw.csv', zwirki_i_wigury = FALSE) 
 dane_dolipca2017[,"Praska sciezka rekreacyjna":=NULL]
 dane_dolipca2017[,"Piesi":=NULL]
 setnames(dane_dolipca2017, "Rowery", grep("Praska", names(dane_dolutego2017), value = TRUE))
@@ -56,7 +56,7 @@ listy_stylow<-zrob_listy_stylow(nazwy) #w obsluga_sumowania
 write.csv(listy_stylow, file = "listy_stylow.csv", fileEncoding = 'UTF-8', row.names = F)
 write.csv(dane_zsumowane, file = "dane/dane_polaczone_zsumowane.csv", fileEncoding = 'UTF-8')
 dane_zsumowane<-dodaj_pogode(dane_zsumowane)
-write.csv(dane_zsumowane, file = "dane/dane_zsumowane_z_pogoda.csv", fileEncoding = 'UTF-8')
+write.csv(dane_zsumowane, file = "dane_zsumowane_z_pogoda.csv", fileEncoding = 'UTF-8')
 dane_long<-wide_to_long(dane_zsumowane)
 write.csv(dane_long, file = "dane_long.csv", fileEncoding = 'UTF-8')
 
@@ -64,7 +64,7 @@ write.csv(dane_long, file = "dane_long.csv", fileEncoding = 'UTF-8')
 
 #dane godzinowe
 #pilotaz
-godz_wybrane_2017<-zaladuj_dane_godzinowe('dane_zrodlowe/2017_08_05-14_06.csv', format="%y-%m-%d %H:%M", ziw=F)
+godz_wybrane_2017<-zaladuj_dane_godzinowe('dane/2017_08_05-14_06.csv', format="%y-%m-%d %H:%M", ziw=F)
 godz_wybrane_2017<-suma_licznikow(godz_wybrane_2017)
 nazwy_licznikow<-names(godz_wybrane_2017)[4:27]
 godz_wybrane_2017[,Jaki_dzien:=weekend(Data)]
