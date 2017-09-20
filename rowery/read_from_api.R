9c615253e2e29afd8e6eac0e8f66ba7ff6d0aca0
Monika Pawlowska <github@greenelephant.pl>
2017-09-20 11:23:21 +0200

    updated API - Marsa

--- rowery/read_from_api.R
+++ rowery/read_from_api.R
@@ -15,8 +15,10 @@ zaladuj_dane_api<-function(filename="bike_counts.csv", ids=ids, od="2017-07-14")
   #txt<- getURL(link)
   tabela<-data.table(read.csv(text=txt, sep=',', header=FALSE))
   setnames(tabela, c("Licznik", "Data", "Liczba_rowerow"))
+  tabela<-tabela[Licznik!=100038471 & Licznik!=100038438]
   tabela[,Data:=as.Date(Data)]
   tabela[,Miejsce:=as.character(Licznik)]
+  t<-unlist(ids[tabela$Miejsce])
   tabela[,Miejsce:=unlist(ids[Miejsce])]
   tabela_wide<-dcast(tabela, Data ~Miejsce, value.var="Liczba_rowerow")
   tabela_wide
