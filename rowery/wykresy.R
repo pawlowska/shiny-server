326c2287723ce9e33528b009322631667c074a0f
Monika Pawlowska <github@greenelephant.pl>
2018-09-07 12:32:32 +0200

    ulepszony wykres weekendow

--- rowery/wykresy.R
+++ rowery/wykresy.R
@@ -1,4 +1,4 @@
-library(RColorBrewer)
+#library(RColorBrewer)
 library(ggplot2)
 library(scales) #for nicer y axis
 library(grid)
@@ -52,19 +52,97 @@ lista_weekendow<-function(dane) {
   }
   l<-data.table(soboty=soboty, niedziele=niedziele)
   l
-  
 }
 
+lista_wolnych<-function(dane) {
+  daty<-dane[,Data]
+  soboty<-daty[weekdays(daty) %in% c("sobota","Saturday", "Sat")]
+  niedziele<-daty[weekdays(daty) %in% c("niedziela","Sunday", "Sun")]
+  dni<-c(soboty, niedziele)
+  wolne<-data.table(Data=dni)
+  wolne[,Start_wolnego:=Data-12*60*60+1]
+  wolne[,Stop_wolnego:=Data+12*60*60]
+  wolne
+}
+
+dodaj_wolne<-function(g, wolne) {
+  if(nrow(wolne)>0) {
+    g<-g+geom_rect(data=wolne, aes(xmin=Start_wolnego, xmax=Stop_wolnego, ymin=-Inf, ymax=+Inf), 
+                   fill='gray', alpha=0.2) +
+      theme( # remove the vertical grid lines
+        panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank())
+  }
+  g
+}
+
+#wykres kilku kolumn
+# wykres_kilka_stary<-function(dane, start, stop, paleta, linie, alfy, krok=1, wartosc='bezwzględne') {
+# #dane w formacie long do łatwiejszego wyboru grup
+#   start=min(c(start,dane[,Data]))
+#   
+#   #x data range and ticks  
+#   zakres_dat=interval(start, stop)
+#   breaks<-better_ticks(zakres_dat, krok)
+# 
+#   #set theme    
+#   theme_set(theme_light(base_size = rozmiar_czcionki))
+#   
+#   #start plot
+#   g<-ggplot(dane)
+#   
+#   if (wartosc=='bezwzględne') {
+#     g<-g+geom_line(aes(Data, Liczba_rowerow, colour=Miejsce, linetype=Miejsce),size=0.7)
+#     if(krok>30) {g<-g+geom_point(aes(Data, Liczba_rowerow, colour=Miejsce))}
+#   }
+#   else
+#     g<-g+geom_area(aes(Data, Liczba_rowerow,  fill=Miejsce, linetype=Miejsce, alpha=Miejsce))
+#   
+#   #show weekends only for the daily plot
+#   if(krok==1) { 
+#     lista<-lista_weekendow(dane)
+#     if(nrow(lista)>0) {
+#       g<-g+geom_rect(data=lista, 
+#                      aes(xmin=soboty, xmax=niedziele, ymin=-Inf, ymax=+Inf), 
+#                      fill='gray', alpha=0.2) +
+#         theme( # remove the vertical grid lines
+#           panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank())
+#     }
+#   } 
+#   
+#   #oś x
+#   g<-g+scale_x_date(date_breaks = breaks, labels=labelsy(krok),limits=c(min(start),max(stop)),
+#                     expand=c(0,0)) #numer X ticks
+#   #oś y
+#   g<-g+scale_y_continuous(breaks = pretty_breaks(7), labels=comma_format())
+#   
+#   g<-g+theme(axis.text.x = element_text(angle = 45, hjust = 1),
+#     legend.position="bottom", legend.margin=margin(0, -2, 0, 1, "cm"))
+#   
+#   #colours and line types
+#   g<-g+scale_linetype_manual(values=linie)+
+#        scale_colour_manual(values=paleta)+
+#        scale_fill_manual(values=paleta)+
+#        scale_alpha_manual(values = alfy)
+#     
+#   #axis labels
+#   g<-g+xlab("Data")+ylab("")
+# 
+#   g<-g+guides(col = guide_legend(byrow = TRUE))
+#   #g<-g+guides(col = guide_legend(ncol=4))
+#   
+#   g
+# }
 
 #wykres kilku kolumn
 wykres_kilka<-function(dane, start, stop, paleta, linie, alfy, krok=1, wartosc='bezwzględne') {
-#dane w formacie long do łatwiejszego wyboru grup
-  start=min(c(start,dane[,Data]))
-    
-  #x data range and ticks  
-  zakres_dat=interval(start, stop)
-  breaks<-better_ticks(zakres_dat, krok)
-
+  
+  dane[,Data:=as.POSIXct(paste(Data,"00:00:00"))]
+  
+  start_osi_x=min(c(as.POSIXct(start),dane[,Data]))
+  
+  #x ticks  
+  breaks<-better_ticks(interval(start, stop), krok)
+  
   #set theme    
   theme_set(theme_light(base_size = rozmiar_czcionki))
   
@@ -80,34 +158,28 @@ wykres_kilka<-function(dane, start, stop, paleta, linie, alfy, krok=1, wartosc='
   
   #show weekends only for the daily plot
   if(krok==1) { 
-    lista<-lista_weekendow(dane)
-    if(nrow(lista)>0) {
-      g<-g+geom_rect(data=lista, 
-                     aes(xmin=soboty, xmax=niedziele, ymin=-Inf, ymax=+Inf), 
-                     fill='gray', alpha=0.2) +
-        theme( # remove the vertical grid lines
-          panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank())
-    }
+    wolne<-lista_wolnych(dane)
+    g<-dodaj_wolne(g, wolne)
   } 
   
   #oś x
-  g<-g+scale_x_date(date_breaks = breaks, labels=labelsy(krok),limits=c(min(start),max(stop)),
+  g<-g+scale_x_datetime(date_breaks = breaks, labels=labelsy(krok), limits=c(start_osi_x, as.POSIXct(stop)),
                     expand=c(0,0)) #numer X ticks
   #oś y
   g<-g+scale_y_continuous(breaks = pretty_breaks(7), labels=comma_format())
   
   g<-g+theme(axis.text.x = element_text(angle = 45, hjust = 1),
-    legend.position="bottom", legend.margin=margin(0, -2, 0, 1, "cm"))
+             legend.position="bottom", legend.margin=margin(0, -2, 0, 1, "cm"))
   
   #colours and line types
   g<-g+scale_linetype_manual(values=linie)+
-       scale_colour_manual(values=paleta)+
-       scale_fill_manual(values=paleta)+
-       scale_alpha_manual(values = alfy)
-    
+    scale_colour_manual(values=paleta)+
+    scale_fill_manual(values=paleta)+
+    scale_alpha_manual(values = alfy)
+  
   #axis labels
   g<-g+xlab("Data")+ylab("")
-
+  
   g<-g+guides(col = guide_legend(byrow = TRUE))
   #g<-g+guides(col = guide_legend(ncol=4))
   
@@ -154,68 +226,42 @@ pogoda_basic<-function(dane, paleta) {
   gg
 }
 
-pogoda_opady<-function(dane, paleta) {
-  #set theme    
-  theme_set(theme_light(base_size = rozmiar_czcionki))
-  
-  method_fit<-ifelse(length(unique(dane[,Data]))<100, "lm", "loess")
-  
-  gg<-ggplot(dane, 
-             aes(deszcz, Liczba_rowerow, colour = Miejsce, shape = Jaki_dzien, size=temp_avg)) +
-    geom_point(alpha=0.8) +
-    scale_size_continuous(range = c(0.01, 8), limits = c(5,25)) +
-    scale_shape_manual(values=c(16,1)) + #full and empty circles
-    scale_x_continuous(breaks=pretty_breaks(8), expand=c(0, 1)) +
-    scale_y_continuous(breaks=pretty_breaks(8), limits = c(-20, NA)) +
-    geom_smooth(size=0.7, alpha=0.2, span = 0.5, method=method_fit) +   # Add a loess smoothed fit curve with confidence region
-    theme(legend.position="bottom", legend.justification="left", legend.box.just = "left",
-          legend.margin=margin(0, -2, 0, 1, "cm"), legend.box="vertical")+
-    scale_colour_manual(values=paleta) +
-    xlab("Opady (mm)")+ylab("")+ #axis labels
-    guides(shape = guide_legend(title="Jaki dzień", order = 1),
-           size = guide_legend(title="Temperatura", order = 2),
-           colour = guide_legend(ncol = 3, byrow = TRUE, order = 3))
-  
-  gg
-}
 
 wykres_pogody_w_czasie<-function(dane) {
   #set theme    
   theme_set(theme_light(base_size = rozmiar_czcionki))
   
+  dane[,Data:=as.POSIXct(paste(Data,"00:00:00"))]
+  
+  wolne<-lista_wolnych(dane)
+  
+  #temperatura
   g_t<-ggplot(dane)+
     geom_line( aes(Data, temp_avg), colour='red3', size=0.5 )+
     geom_ribbon(aes(x=Data, ymin=temp_min, ymax=temp_max), fill='red', alpha=0.1)+
-    scale_x_date(expand=c(0,0), labels=NULL)+ #numer X ticks
+    scale_x_datetime(expand=c(0,0), labels=NULL)+ #numer X ticks
     ylab("Temperatura (°C)")+xlab("")
   
+  #opady
   g_d<-ggplot(dane)+
     geom_segment(aes(x=Data, xend=Data, y=0, yend=deszcz+snieg, color=Rodzaj_opadu), 
                  lineend = "butt", size=1)+
-    scale_x_date(expand=c(0,0), labels=NULL)+ #numer X ticks
+    scale_x_datetime(expand=c(0,0), labels=NULL)+ #numer X ticks
     scale_color_manual(values = c("midnightblue","deepskyblue"))+
     ylab("Opady (mm)")+xlab("")+theme(legend.position="none")
-  
-  
-  lista<-lista_weekendow(dane)
-  
+    
   plot_list=list(g_t, g_d)
   for (i in 1:2) {
-      plot_list[[i]]<-plot_list[[i]]+geom_rect(data=lista, 
-                   aes(xmin=soboty, xmax=niedziele, ymin=-Inf, ymax=+Inf), 
-                   fill='gray', 
-                   alpha=0.2) +
-      theme( # remove the vertical grid lines
-        panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank())+
-      theme(plot.margin = unit(c(0,0,-5,0), "pt"))
+      plot_list[[i]]<-dodaj_wolne(plot_list[[i]], wolne) + theme(plot.margin = unit(c(0,0,-5,0), "pt"))
   }
   plot_list
 }
 
+#combine plots of weather and bikes number
 wykres_pogoda_liczba<-function(dane, start, stop, paleta, linie) {
   lista<- wykres_pogody_w_czasie(dane)
   g2<- wykres_kilka(dane, start, stop, paleta, linie)
   grid.newpage()
-  grid.draw(rbind(ggplotGrob(lista[[1]]), ggplotGrob(lista[[2]]),
-                   ggplotGrob(g2), size = "last"))
+  grid.draw(rbind(ggplotGrob(lista[[1]]), ggplotGrob(lista[[2]]), 
+                  ggplotGrob(g2), size = "last"))
 }
